4a695302bd5a1a0578bdedb40532fdfa52f257a5
MIR <mightykiwi@yahoo.com>
2014-08-19 21:19:39 -0700

    add nomalization of power name before save

--- lib/power.rb
+++ lib/power.rb
@@ -1,4 +1,11 @@
 class Power < ActiveRecord::Base
   validates :name, presence: true
+  before_save :normalize_name
   has_many :characters
+
+private
+  def normalize_name
+    self.name = self.name.downcase.titleize
+  end
+
 end
--- spec/power_spec.rb
+++ spec/power_spec.rb
@@ -5,4 +5,7 @@ describe Power do
   it { should validate_presence_of :name }
   it { should have_many :characters}
 
+  it 'should downcase and capitalize names' do
+    expect(Character.create({name: 'strength'}).name).to eq 'Strength'
+  end
 end
