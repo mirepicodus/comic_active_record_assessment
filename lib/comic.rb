fc0c64020a0720d17c93f97e144d611350caa6d6
MIR <mightykiwi@yahoo.com>
2014-08-19 21:16:40 -0700

    add nomalization of comic name before save

--- lib/comic.rb
+++ lib/comic.rb
@@ -1,4 +1,11 @@
 class Comic < ActiveRecord::Base
   validates :name, presence: true
+  before_save :normalize_name
   has_and_belongs_to_many :characters
+
+private
+  def normalize_name
+    self.name = self.name.downcase.titleize
+  end
+
 end
--- spec/comic_spec.rb
+++ spec/comic_spec.rb
@@ -4,4 +4,8 @@ describe Comic do
 
   it { should validate_presence_of :name }
   it { should have_and_belong_to_many :characters }
+
+  it 'should downcase and capitalize names' do
+    expect(Comic.create({name: 'hulk'}).name).to eq 'Hulk'
+  end
 end
