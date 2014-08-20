c73462d753bb95eb88fe0c9ac063f712120e5b86
MIR <mightykiwi@yahoo.com>
2014-08-19 20:48:50 -0700

    add reltion from comic to characters

--- lib/comic.rb
+++ lib/comic.rb
@@ -1,3 +1,4 @@
 class Comic < ActiveRecord::Base
   validates :name, presence: true
+  has_and_belongs_to_many :characters
 end
--- spec/comic_spec.rb
+++ spec/comic_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe Comic do
 
   it { should validate_presence_of :name }
-
+  it { should have_and_belong_to_many :characters }
 end
