9230533f90376f8fcba929bfafbb6b991dc5ca17
MarkR <student@epicodus.com>
2014-08-19 17:35:42 -0700

    intial commit with basic config

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require(:default, :test)
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+database_configurations = YAML::load(File.open('./db/config.yml'))
+test_configuration = database_configurations['test']
+ActiveRecord::Base.establish_connection(test_configuration)
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Comic.all.each { |row| row.destroy }
+    Character.all.each { |row| row.destroy }
+    Power.all.each { |row| row.destroy }
+  end
+end
